421a6aeb7aa0afed923c4cdb65c9809bb208c958
piklin <hplesson@gmail.com>
2020-07-24 03:18:24 -0400

    MVP goals completed, I think :expressionless::question:

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ const petApp = {};
 
 $(function() {
 
+    $('button').on('click', function(event) {
+    
+        $('html, body').animate( {
+            scrollTop: $('main').offset().top,
+        })
+    });
+
     // On change, grab the selected item from input
     $('input').on('change', function(event) {
         // console.log(this);
 
         const grabItem = $(this).val();
-        console.log(grabItem);
+        // console.log(grabItem);
 
         // $(`.${grabItem}`) connects the value to a class 
         // '.' + grabItem (concat) and $`.${grabItem}` (temlit) is the same thing
@@ -22,27 +29,3 @@ $(function() {
 
     })
 });
-
-
-// PSEUDO CODE
-// DONE // One big form that has listeners (on.Change) for each input field  
-// DONE // Radial buttons, and apply (on.Change) on input field for each section
-// DONE // Two options to switch the items and pets:
-// a)	addClass or removeClass (display: none by default on the elements) 
-// b)	jQuery method show/hide to hide the elements (e.g. hide: crown, etc)
-// Will be using the if / else conditional statement to check and pass the value
-
-
-// TEST DIFFERENT METHODS
-
-// const showItem = $(grabItem).addClass('.imageVisible');
-// console.log(showItem);
-
-// Grab the value that is selected e.g value=dog will show in console log 'dog'
-// Save the code below into variable (const) to make it reusable. 
-// Use const instead of namespace (not needed) because the value upon 'click/change'
-
-// if ( $(this).attr('name') === 'pickPet') {
-        //     $('.petOptions .imageVisible').removeClass('imageVisible');
-        
-        // }
